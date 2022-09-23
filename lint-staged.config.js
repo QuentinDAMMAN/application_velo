48ca8968b9bb2e2648abe6a8edac36335dd70d99
Quentin DAMMAN <quentindamman@gmail.com>
2022-09-23 10:46:02 +0200

    ajout linter-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "*.{js,json,md,mdx,html,css,scss}": "prettier --write",
+  /*arrowParens: 'always',
+    jsxSingleQuote: true,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'es5'*/
+};
